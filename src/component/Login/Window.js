82bacfc5df474a439c51e020f5a0d9e33839ae3a
DysonBL <henrydysonj@dorustree.in>
2022-03-24 19:26:09 +0530

    new

--- src/component/Login/Window.js
+++ src/component/Login/Window.js
@@ -1,59 +1,71 @@
 import { Paper } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState ,useReducer} from "react";
 import "./Window.css"
 import { Table } from "@mui/material";
-import { useReducer } from "react";
 import { Button } from "@mui/material";
+import Pop from './Pop';
 
 const Reducer = (state, action) => {
-    console.log("hii",action)
+    console.log("data",action)
     switch (action.type) {
         case "add":
-            return
-             [...state, ...action.payload]
+            return [...state, ...action.payload]
             
         case "del":
-            return state.filter(state=>state.newDatas!==action.payload.id)
+            return state.filter(user=>user.id!==action.payload)
+        case "edit":
+            return 
         default :
         return state
     } 
     
 } 
+
    const Window = () => {
-        const [state, dispatch] = useReducer(Reducer, [])
-        const [Name, setName] = ('')
+        const [state, dispatch]=useReducer(Reducer, [])
+        const [Name, setName]=useState('')
+
+        const [show, setShow] = useState(false);
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+
         useEffect(() => {
             let Data = localStorage.getItem("data")
             let newDatas = JSON.parse(Data);
             dispatch({ type:"add",payload:newDatas})
         }, [])
+       
 
-const handleSubmit=(value)=>{
-    dispatch({type:"del",payload:value.id})
+const handleSubmit=(newDatas)=>{
+    dispatch({type:"del",payload:newDatas.id})
 }
-
-        return (
+const onsubmit=()=>{
+    handleShow()
+}
+      return (
             <>
                 <h2>Wellcome User</h2>
                 <Paper>
-                    <Table striped bordered hover>
+                    <Table>
                         <thead>
                             <tr>
+                                <th>Id.No</th>
                                 <th>First Name</th>
                                 <th>Second Name</th>
                                 <th>Email</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {state.map((value) => {
+                            {state.map((newDatas) => {
                                 return (
                                     <><tr>
-                                        <td>{value.Fname}</td>
-                                        <td>{value.Sname}</td>
-                                        <td>{value.Email}</td>
-
-                                        <td><Button className={"del"} onClick={()=>handleSubmit(value)}> DEL</Button></td>
-                                        <td><Button className={"edit"} onClick={onsubmit}> EDIT</Button></td>
+                                        <td>{newDatas.id}</td>
+                                        <td>{newDatas.Fname}</td>
+                                        <td>{newDatas.Sname}</td>
+                                        <td>{newDatas.Email}</td>
+                                        <td><Button className={"del"} onClick={()=>handleSubmit(newDatas)}> DEL</Button></td>
+                                        <td><Button className={"edit"} onClick={onsubmit}>EDIT</Button></td>
+                                        <Pop open={show} close={handleClose}/>
                                     </tr>
                                     </>)
                             })}
