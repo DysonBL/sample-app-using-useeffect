85e35ea5b725d3b02c22e7419f0441b6247b87de
DysonBL <henrydysonj@dorustree.in>
2022-03-25 21:09:16 +0530

    dd

--- src/component/Login/Pop.js
+++ src/component/Login/Pop.js
@@ -1,33 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Button } from "react-bootstrap";
 import { Input } from '@mui/material';
 
-
 const Pop=(props)=>{
+  console.log("testforpropss",props)
+  const [data,setdata]=useState({
+      id :"",
+      Fname :"",
+      Sname :"",
+      Email :"",
+  })
+   console.log(data,"iddata")
+   console.log(props,"datasss")
 
+  const handleChange=(e)=>{
+    console.log(data,"p1") 
+    setdata({...data,[e.target.name]:e.target.value})
+    
+  }
 
-const[Name,setName]=useState({
-  FirstName:"",
-  SecondName:"",
-  Email :""
-})
-  return (
+  useEffect (()=>{
+      if(props.userData){
+     setdata(props.userData) 
+      }
+  },[props.userData]);
+  
+  const handleSave=()=>{
+     props.edit(data)
+     console.log("datas--==>",data)
+   }
+
+ return (
     <>
       <Modal show={props.open} onHide={props.close}>
         <Modal.Header closeButton>
-          <Modal.Title>Edit User..</Modal.Title>
+          <Modal.Title>userData User..</Modal.Title>
         </Modal.Header>
         <Modal.Body> 
-          <div><Input type="text" placeholder='Enater First name'>First Name</Input></div>
-         <div> <Input type="text" placeholder='Enater Second name'>Second Name</Input></div>
-         <div> <Input type="text" placeholder='Enater Email ID'>Email</Input></div>
+         <div><Input type="text" placeholder='Enter First name' onChange={handleChange} value={data.Fname} name='Fname'>First Name</Input></div>
+         <div> <Input type="text" placeholder='Enter Second name'onChange={handleChange} value={data.Sname} name='Sname'>Second Name</Input></div>
+         <div> <Input type="text" placeholder='Enter Email ID'   onChange={handleChange} value={data.Email} name='Email'>Email</Input></div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={props.close}>
             Close
           </Button>
-          <Button variant="primary" onClick={props.close}>
+          <Button variant="primary" onClick={handleSave}>
             Save User
           </Button>
         </Modal.Footer>
@@ -35,4 +54,4 @@ const[Name,setName]=useState({
     </>
   );
 }
-export default Pop
+export default Pop;
--- src/component/Login/Window.js
+++ src/component/Login/Window.js
@@ -1,78 +1,103 @@
 import { Paper } from "@mui/material";
-import React, { useEffect, useState ,useReducer} from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import "./Window.css"
 import { Table } from "@mui/material";
 import { Button } from "@mui/material";
 import Pop from './Pop';
 
 const Reducer = (state, action) => {
-    console.log("data",action)
+    console.log("state--==>",state)
     switch (action.type) {
         case "add":
+            console.log(action,"d1")
             return [...state, ...action.payload]
             
         case "del":
-            return state.filter(user=>user.id!==action.payload)
+            console.log(state,"d2")
+            return state.filter(user => user.id !== action.payload)
+            
         case "edit":
-            return 
-        default :
-        return state
-    } 
-    
-} 
+            console.log(state,"d3")
+             let index=state.findIndex(user=>user.Id ===action.payload.Id)
+             state.splice(index,1,action.payload)  
+              
+          return state 
 
-   const Window = () => {
-        const [state, dispatch]=useReducer(Reducer, [])
-        const [Name, setName]=useState('')
-
-        const [show, setShow] = useState(false);
-        const handleClose = () => setShow(false);
-        const handleShow = () => setShow(true);
-
-        useEffect(() => {
-            let Data = localStorage.getItem("data")
-            let newDatas = JSON.parse(Data);
-            dispatch({ type:"add",payload:newDatas})
-        }, [])
-       
-
-const handleSubmit=(newDatas)=>{
-    dispatch({type:"del",payload:newDatas.id})
-}
-const onsubmit=()=>{
-    handleShow()
-}
-      return (
-            <>
-                <h2>Wellcome User</h2>
-                <Paper>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Id.No</th>
-                                <th>First Name</th>
-                                <th>Second Name</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {state.map((newDatas) => {
-                                return (
-                                    <><tr>
-                                        <td>{newDatas.id}</td>
-                                        <td>{newDatas.Fname}</td>
-                                        <td>{newDatas.Sname}</td>
-                                        <td>{newDatas.Email}</td>
-                                        <td><Button className={"del"} onClick={()=>handleSubmit(newDatas)}> DEL</Button></td>
-                                        <td><Button className={"edit"} onClick={onsubmit}>EDIT</Button></td>
-                                        <Pop open={show} close={handleClose}/>
-                                    </tr>
-                                    </>)
-                            })}
-                        </tbody>
-                    </Table>
-                </Paper>
-            </>
-        )
+        default:
+            return state
     }
-    export default Window
+
+}
+
+const Window = () => {
+    const [state, dispatch] = useReducer(Reducer, [])
+    const [Name, setName] = useState('')
+    const [newDatas,setnewDatas] = useState({})
+    const [userDetail,setUserDetail] = useState("")
+    console.log("userDetail",userDetail)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    useEffect(() => {
+        let Data = localStorage.getItem("data")
+        let newDatas = JSON.parse(Data);
+        console.log(state,"d4")   
+        dispatch({ type: "add", payload: newDatas })
+    }, [])
+ 
+    const handleSubmit = (newDatas) => {
+        console.log(state,"d5")   
+        dispatch({ type: "del", payload: newDatas.id })
+    }
+    
+    const onsubmit = (e) => {
+        console.log("testfore",e)
+        handleShow()    
+        setUserDetail(e)              
+    }
+const handleEdit=()=>{
+    dispatch({ type: "edit", payload:newDatas}) 
+}
+    // const handleSave = (dataR) =>{
+    //     console.log("testForDataR",dataR)
+    //     setShow(false)
+    // }
+
+    return (
+        <>
+            <h2>Wellcome User</h2>
+            <Paper>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Id.No</th>
+                            <th>First Name</th>
+                            <th>Second Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {state.map((newDatas) => {
+                            return (
+                                <><tr>
+                                    <td>{newDatas.id}</td>
+                                    <td>{newDatas.Fname}</td>
+                                    <td>{newDatas.Sname}</td>
+                                    <td>{newDatas.Email}</td>
+                                    <td><Button className={"del"} onClick={() => handleSubmit(newDatas)}> DEL</Button></td>
+                                    <td><Button className={"edit"} onClick={() => onsubmit(newDatas)}>EDIT</Button></td>
+                                   
+                                    </tr>
+                                </>)
+                                })}
+                    </tbody>
+                   
+                  </Table>
+            </Paper>
+            <Pop open={show} close={handleClose} edit={handleEdit} userData={userDetail}/>
+        </>
+    )
+}
+
+export default Window
